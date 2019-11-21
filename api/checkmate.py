d6a41842d129139ca43a42ebcbedbbadbe1b36ae
Mohammed Riaz Khan <riaz_khan96@outlook.com>
2019-11-21 20:59:31 +0200

    Added HTML Templates for Views

--- api/checkmate.py
+++ api/checkmate.py
@@ -8,31 +8,48 @@ app = Flask(__name__, template_folder="../web/templates", static_folder="../web/
 def hello():
     return render_template('index.html')
 
+@app.route("/run", methods=['POST'])
+def all_tests():
+    if request.method == 'POST':
+        checks_to_run = request.form.getlist('checks')
+        print(len(checks_to_run))
+        for i in range(len(checks_to_run)):
+            if checks_to_run[i] == 'ping':
+                print('running ping')
+            elif checks_to_run[i] == 'traceroute':
+                print('running traceroute')
+            elif checks_to_run[i] == 'nslookup':
+                print('running nslookup')
+            elif checks_to_run[i] == 'telnet':
+                print('running telnet')
+
+    return jsonify('Running Tests')
+
 @app.route("/ping", methods=['POST'])
 def ping_host():
     if request.method == 'POST':
-        endpoint = request.form['host']
+        endpoint = request.form['endpoint']
         output = ping.perform_ping(endpoint)
         return render_template('index.html', output=output)
 
 @app.route("/telnet", methods=['POST'])
 def telnet_host():
     if request.method == 'POST':
-        endpoint = request.form['host']
+        endpoint = request.form['endpoint']
         output = telnet.check_if_port_open(host=endpoint, port=22)
         return render_template('index.html', output=output)
 
 @app.route("/traceroute", methods=['POST'])
 def traceroute_host():
     if request.method == 'POST':
-        endpoint = request.form['host']
+        endpoint = request.form['endpoint']
         output = traceroute.trace_route_of_instance(endpoint)
         return render_template('index.html', output=output)
 
 @app.route("/nslookup", methods=['POST'])
 def nslookup_host():
     if request.method == 'POST':
-        endpoint = request.form['host']
+        endpoint = request.form['endpoint']
         output = nslookup.name_to_ip(endpoint)
         return render_template('index.html', output=output)
 
